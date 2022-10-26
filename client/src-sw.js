ffe36b7f0e518c005282cd1181ef107c57a18476
AyalaStonic <ayaladuvan876@gmail.com>
2022-10-26 16:22:29 -0400

    Fixing all files and testing them

--- client/src-sw.js
+++ client/src-sw.js
@@ -26,14 +26,15 @@ warmStrategyCache({
 
 registerRoute(({ request }) => request.mode === 'navigate', pageCache);
 
+// TODO: Implement asset caching
 registerRoute(
-  ({ request }) => ["style", "script", "worker"].includes(request.destination),
+  ({ request }) => ['style', 'script', 'worker'].includes(request.destination),
   new StaleWhileRevalidate({
-    cacheName: "asset-cache",
+    cacheName: 'asset-cache',
     plugins: [
       new CacheableResponsePlugin({
         statuses: [0, 200],
       }),
     ],
-  })
+  })  
 );
